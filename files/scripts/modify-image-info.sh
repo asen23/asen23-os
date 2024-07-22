08c1fb274b44d98f9dba784269df3cbc1dbb75da
asen23 <andersenlim23@gmail.com>
2024-07-22 21:47:38 +0700

    ref: use yq to parse image-info

--- files/scripts/modify-image-info.sh
+++ files/scripts/modify-image-info.sh
@@ -6,7 +6,9 @@
 set -oue pipefail
 
 # Edit image-info.json
-# Change bazzite to azurite
-sed -i 's/bazzite/azurite/' /usr/share/ublue-os/image-info.json
-# Change vendor to asen23
-sed -i 's/ublue-os/asen23/' /usr/share/ublue-os/image-info.json
+# Change image name
+yq -i '.image-name = "azurite"' /usr/share/ublue-os/image-info.json
+# Change vendor
+yq -i '.image-vendor = "asen23"' /usr/share/ublue-os/image-info.json
+# Change image ref
+yq -i '.image-ref = "ostree-image-signed:docker://ghcr.io/asen23/azurite"' /usr/share/ublue-os/image-info.json
