09f79f1430f61600b34c2b0b86945ed9e294ce87
asen23 <andersenlim23@gmail.com>
2024-07-23 14:46:05 +0700

    chore: remove &&

--- files/scripts/modify-os-release.sh
+++ files/scripts/modify-os-release.sh
@@ -6,11 +6,11 @@
 set -oue pipefail
 
 # Edit os-release
-sed -i '/^PRETTY_NAME/s/Bazzite/Azurite/' /usr/lib/os-release &&\
-sed -i 's/^NAME=.*/NAME="Azurite"/' /usr/lib/os-release && \
-sed -i 's|^CPE_NAME="CPE_NAME="cpe:/o:universal-blue:bazzite|CPE_NAME="cpe:/o:asen23:azurite|' /usr/lib/os-release && \
-sed -i 's/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME="azurite"/' /usr/lib/os-release && \
-sed -i 's/^VERSION_CODENAME=.*/NAME="Rhea"/' /usr/lib/os-release && \
-sed -i 's/^ID=.*/ID=azurite/' /usr/lib/os-release && \
-sed -i "s/^BUILD_ID=.*/BUILD_ID=$SHA_HEAD_SHORT/" /usr/lib/os-release && \
-sed -i 's/VARIANT_ID.*/VARIANT_ID=azurite/' /usr/lib/os-release
+sed -i '/^PRETTY_NAME/s/Bazzite/Azurite/' /usr/lib/os-release
+sed -i 's/^NAME=.*/NAME="Azurite"/' /usr/lib/os-release
+sed -i 's|^CPE_NAME="CPE_NAME="cpe:/o:universal-blue:bazzite|CPE_NAME="cpe:/o:asen23:azurite|' /usr/lib/os-release
+sed -i 's/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME="azurite"/' /usr/lib/os-release
+sed -i 's/^VERSION_CODENAME=.*/NAME="Rhea"/' /usr/lib/os-release
+sed -i 's/^ID=.*/ID=azurite/' /usr/lib/os-release
+sed -i "s/^BUILD_ID=.*/BUILD_ID=$SHA_HEAD_SHORT/" /usr/lib/os-release
+sed -i 's/^VARIANT_ID.*/VARIANT_ID=azurite/' /usr/lib/os-release
