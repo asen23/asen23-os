629f8a84869eaf426c0e1292ce7007e8e79d5ed9
asen23 <andersenlim23@gmail.com>
2024-07-23 17:43:36 +0700

    fix: modify os release script

--- files/scripts/modify-os-release.sh
+++ files/scripts/modify-os-release.sh
@@ -10,7 +10,7 @@ sed -i '/^PRETTY_NAME/s/Bazzite/Azurite/' /usr/lib/os-release
 sed -i 's/^NAME=.*/NAME="Azurite"/' /usr/lib/os-release
 sed -i 's|^CPE_NAME="CPE_NAME="cpe:/o:universal-blue:bazzite|CPE_NAME="cpe:/o:asen23:azurite|' /usr/lib/os-release
 sed -i 's/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME="azurite"/' /usr/lib/os-release
-sed -i 's/^VERSION_CODENAME=.*/NAME="Rhea"/' /usr/lib/os-release
+sed -i 's/^VERSION_CODENAME=.*/VERSION_CODENAME="Rhea"/' /usr/lib/os-release
 sed -i 's/^ID=.*/ID=azurite/' /usr/lib/os-release
 # sed -i "s/^BUILD_ID=.*/BUILD_ID=${GITHUB_SHA::7}/" /usr/lib/os-release
 sed -i 's/^VARIANT_ID.*/VARIANT_ID=azurite/' /usr/lib/os-release
