fea503219eefd774201a9f56ef4eda346da15dd0
asen23 <andersenlim23@gmail.com>
2024-12-16 20:41:47 +0700

    feat: modify bazzite image ref

---
+++ files/scripts/bazzite/modify-image-info.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Edit image-info.json
+cat /usr/share/ublue-os/image-info.json | \
+# Change image ref \
+jq '."image-ref" = "ostree-image-signed:docker://ghcr.io/asen23/azurite"' > /usr/share/ublue-os/image-info.json
+
